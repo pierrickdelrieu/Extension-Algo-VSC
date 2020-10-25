5af9ba1c2f1a55b1bd1ac57f99966d752cd96f1e
pierrickdelrieu <pierrick.delrieu@efrei.net>
2020-10-25 21:04:55 +0100

    v. 1.0.0

--- extension.ts
+++ extension.ts
@@ -8,16 +8,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "picalgo" is now active!');
+	console.log('Congratulations, your extension "pic-algo" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('picalgo.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('pic-algo.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from PicAlgo!');
+		vscode.window.showInformationMessage("'Pic Algo' est une extension pour le language algorithmique!");
 	});
 
 	context.subscriptions.push(disposable);
